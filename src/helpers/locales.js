96c460393c0c2f5221a3deb51b631740e702a953
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2018-12-16 21:40:59 +0800

    create helper

---
+++ src/helpers/locales.js
@@ -0,0 +1,10 @@
+import enUS from '../assets/locales/en-US.json';
+import fr from '../assets/locales/fr.json';
+
+// Insert additional string objects here
+const locales = {
+  'en-US': enUS,
+  fr: fr,
+};
+
+export default locales;
